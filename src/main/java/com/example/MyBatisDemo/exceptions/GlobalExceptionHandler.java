1406c551395e91f6c5933bcd319161d3fb900008
I3-Parth <parth.shah@techprescient.com>
2024-03-17 14:41:54 +0530

    f2: PR - Naming Conventions corrected

--- src/main/java/com/example/MyBatisDemo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/MyBatisDemo/exceptions/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> studentNOtFoundException(ResourceNotFoundException ex, WebRequest request){
+    public ResponseEntity<?> studentNotFoundException(ResourceNotFoundException ex, WebRequest request){
         ErrorDetails errorDetails=new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
